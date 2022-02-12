efd4c9bc9e91ccadffe24d4f97f1d8e79409ce8e
Marcel Rädeker <marcel.raedeker@gamil.com>
2022-02-13 00:07:55 +0100

    Added implementation skeleton

--- cjson.c
+++ cjson.c
@@ -4,4 +4,80 @@
 
 #include "cjson.h"
 
+cjsonObject* cjson__ExtractObject( char*, char**, hmap*, cjsonError* );
+cjsonArray*  cjson__ExtractArray( char*, char**, hmap*, cjsonError* );
+
+_Bool cjson__ExtractAddress( const void* v, unsigned n ){
+    return *((const unsigned long long*)v) & (1ull << n);
+}
+
+_Bool cjson__CompareAddress( const void* a, const void* b ){
+    return *((const unsigned long long*)a) == *((const unsigned long long)b);
+}
+
+cjson_StringAddressMapping* cjson__Address2String( hmap* map, void* adr ){
+    return (cjson__StringAddressMapping*) hmapGet( map, &adr );
+}
+
+_Bool cjson__StringWriteBackDelete( void* v, void* ex ){
+
+    //TODO
+
+    return 1;
+}
+
+_Bool cjson__StringWriteBack( void* v, void* ex ){
+
+    //TODO
+
+    return 1;
+}
+
+hmap* cjson__ExpungeStrings( char* input ){
+
+    //TODO
+
+    return 0;
+}
+
+cjsonError cjson__DeilmCheck( const char* input, unsigned input_sz ){
+
+    //TODO
+
+    return cjsonerr_Unkown;
+}
+
+cjsonDataField* cjsonParse( char* input, unsigned input_sz, cjsonError* error ){
+
+    //TODO
+    
+    return 0;
+}
+
+void cjsonFree( cjsonDataField* datafield ){
+
+    //TODO
+
+}
+
+cjsonObject* cjsonObjectInit(){
+
+}
+
+cjsonArray* cjsonArrayInit( unsigned sz ){
+
+}
+
+cjsonDataField* cjsonGetObjectField( cjsonObject* obj, const char* fieldname ){
+
+    //TODO
+
+    return 0;
+}
+
+cjsonDataField* cjsonGetArrayData( cjsonArray* obj, unsigned n ){
+    void* adr = ((_Bool*)arr) + sizeof(jsonArray) + n * sizeof(cjsonDataField);
+    return (cjsonDataField*)adr;
+}
+
 #endif
--- cjson.h
+++ cjson.h
@@ -5,4 +5,79 @@
 #include "hmap/hmap.c"
 #include "comfystring/comfystr.c"
 
+#define cjson__objdelim0 '{'
+#define cjson__objdelim1 '}'
+#define cjson__arrdelim0 '['
+#define cjson__arrdelim1 ']'
+#define cjson__strdelim0 '\"'
+#define cjson__escapechr '\\'
+#define cjson__unmarkchr 7
+
+typedef enum {
+    cjsonerr_NoError = 0,
+    cjsonerr_ObjDelim,
+    cjsonerr_StrDelim,
+    cjsonerr_ArrDelim,
+    cjsonerr_BadData,
+    cjsonerr_Unkown
+} cjsonError;
+
+typedef enum cjson__Type {
+    cjsontype_Invalid = 0,
+    cjsontype_Object,
+    cjsontype_Array,
+    cjsontype_String,
+    cjsontype_Integer,
+    cjsontype_Double,
+    cjsontype_Bool,
+    cjsontype_Null
+} cjsonType;
+
+struct cjson__Object;
+struct cjson__Array;
+union  cjson__Value;
+
+typedef struct {
+    void* Address;
+    char* String;
+} cjson_StringAddressMapping;
+
+typedef union cjson__Value {
+    _Bool Boolean;
+    double Double;
+    long long Integer;
+    char* String;
+    struct cjson__Array* Array;
+    struct cjson__Object* Object;
+} cjsonValue;
+
+typedef struct {
+    union cjson__Value Value;
+    enum  cjson__Type Type;
+} cjsonDataField;
+
+typedef struct {
+    cjsonDataField Field;
+    char* Name;
+} cjsonObjectField;
+
+typedef struct cjson__Object {
+    unsigned Elements;
+    hmap* Fields;
+} cjsonObject;
+
+typedef struct cjson__Array {
+    unsigned Elements;
+} cjsonArray;
+
+cjsonObject* cjsonObjectInit();
+cjsonArray*  cjsonArrayInit(unsigned);
+
+cjsonDataField* cjsonGetObjectField( cjsonObject*, const char* );
+cjsonDataField* cjsonGetArrayData( cjsonArray*, unsigned );
+
+cjsonDataField* cjsonParse( char*, unsigned, cjsonError* );
+
+void cjsonFree( cjsonDataField* );
+
 #endif
